b495ff6ae5f4674bcf3366af4b5e019b9c3567d2
Kristen <kristenwood123@gmail.com>
2021-11-20 12:48:27 -0800

    typescript compiler and functions

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    if (resultConversion === 'as-numer') {
-        return +result;
-    }
-    else {
-        return result.toString();
-    }
+var userInput;
+// We don't know what it will be
+var userName;
+userInput = 5;
+userInput = 'hello';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combinedStringAges = combine('30', '55', 'as-number');
-console.log(combinedStringAges);
-var combinedNames = combine('Max', ' Anna', 'as-text');
-console.log(combinedNames);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An error occured! ', 500);
