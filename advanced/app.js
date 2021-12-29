ae7800d3bda15a69fe10dbccef1cc9d768228879
Kristen <kristenwood123@gmail.com>
2021-12-29 00:56:54 -0800

    public and private

--- advanced/app.js
+++ advanced/app.js
@@ -1,13 +1,24 @@
 // Classes
 var Department = /** @class */ (function () {
     function Department(n) {
+        this.employees = [];
         this.name = n;
     }
     Department.prototype.describe = function () {
         console.log('Department ' + this.name);
     };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
-var clothing = new Department('Clothing');
-console.log(clothing);
-
+// const clothing = new Department('Clothing')
+var accounting = new Department('Accounting');
+accounting.addEmployee('Frank');
+accounting.addEmployee('Kristen');
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: 's', describe: accounting.describe };
--- advanced/app.ts
+++ advanced/app.ts
@@ -1,21 +1,31 @@
 // Classes
 class Department {
-  name: string;
+  public name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
   }
 
-  describe() {
+  describe(this: Department) {
     console.log('Department ' + this.name);
-    
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee)
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const clothing = new Department('Clothing')
-
-
-clothing.describe()
-
+// const clothing = new Department('Clothing')
+const accounting = new Department('Accounting')
+accounting.addEmployee('Frank')
+accounting.addEmployee('Kristen')
+accounting.printEmployeeInformation()
+// const accountingCopy = { name: 's', describe: accounting.describe };
 
 
